31bde09a9568b669df2dcf63b5a5fd7cfe610c49
mi55a <milymendez678@gmail.com>
2026-07-11 22:36:59 -0500

    API route finished and added form to page.tsx for bridge between community and form API request.

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -41,9 +41,6 @@ export async function POST(request: Request) {
         return NextResponse.json(
             { error: "Unable to create event." },
             { status: 500 },
-        )
-    };
-
-
-
+        );
+    }
 }
